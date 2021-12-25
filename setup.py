714c7a04a251b9c62550bf5e189c077b30e2c881
Cyker Way <cykerway@gmail.com>
2021-12-25 23:05:14 +0000

    -

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='argparse-ext',
-    version='1.4.0',
+    version='1.4.1',
     packages=find_packages(),
 
     url='https://github.com/cykerway/argparse-ext',
